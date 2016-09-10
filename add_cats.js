dbc7c44c559394c21db83f6cc9cb252b88a62c6a
unknown <contact.abeyene@gmail.com>
2016-09-10 17:19:13 +0100

    Added loading of jQuery + structured code a little better + added some comments + added config object making it easier to change

--- add_cats.js
+++ add_cats.js
@@ -1,45 +1,73 @@
+
 /*
-* Add multiple categories from Magento admin dashboard using js from the browser console
+* Add multiple Brand attributes from Magento admin dashboard using js on browser console
+* 
+* Run this in Catalog > Attributes > Manage Attributes > Manage Label/ Options > Brand
 */
 
-function updateCats() {
-	// list of category names (replace with ones you want to add)
-	var cats = [
-		'Category name 1',
-		'Category name 2',
-		'Category name 3',
-		'Category name 4',
-		'Category name 5',
-		'Category name 6',
-		'Category name 7',
-		'Category name 8',
-		'Category name 9',
-		'Category name 10',
-		'Category name 11',
-		'Category name 12',
-		'Category name 13',
-		'Category name 14',
-		'Category name 15',
-		'Category name 16',
-		'Category name 17',
-		'Category name 18',
-		'Category name 19',
-		'Category name 20',
-	];
-	
-	// add category btn
-	var $addCatLink = jQuery('#add_new_option_button');
-	
-	var $inputBox;
-	
-	// loop through all categories and add them
-	for (var i = 0; i <= cats.length; i++) {
-		$inputBox = jQuery('[name="option[value][option_' + (i) + '][0]"]');
-		$inputBox.val(cats[i]);
+(function() {
 
-		$addCatLink.click();
+	var config = {
+		// Change this to reflect the list of your categories 
+		//list of category names (replace with ones you want to add)
+		categories: [
+			'Category name 1',
+			'Category name 2',
+			'Category name 3',
+			'Category name 4',
+			'Category name 5',
+			'Category name 6',
+			'Category name 7',
+			'Category name 8',
+			'Category name 9',
+			'Category name 10'
+		],
+		
+		jqueryLibUrl: "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"
 	}
-}
 
 
-updateCats();
+
+	// load jquery
+	function loadJQuery() {
+		// bail if jQuery already exists
+		if (typeof jQuery != 'undefined') return false;
+		
+		var jq = document.createElement('script');
+		jq.src = config.jqueryLibUrl;
+		document.getElementsByTagName('head')[0].appendChild(jq);
+	}
+
+
+	// add categories
+	function updateCats() {
+		var cats = config.categories;
+		
+		// add category btn
+		var $addCatLink = jQuery('#add_new_option_button');
+		
+		var $inputBox;
+		
+		// loop through all categories and add them - assuming you don't have some values in this already
+		for (var i = 0; i <= cats.length; i++) {
+			$inputBox = jQuery('[name="option[value][option_' + (i) + '][0]"]');
+			$inputBox.val(cats[i]);
+
+			$addCatLink.click();
+		}
+	}
+	
+	
+	function init () {
+		loadJQuery();		
+		
+		// give jquery sometime to load from the server
+		setTimeout(function() {
+			// avoid jQuery confilct
+			jQuery.noConflict();			
+			updateCats();
+		},3000);
+	}
+	
+	init();
+})();
